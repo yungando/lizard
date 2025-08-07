1054e7378bc0da6f90965ec8ad7b93880bddb7ef
James Anderson <joji@ando.ws>
2025-08-07 17:07:01 +0100

    Initial Commit

---
+++ lizard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  lizard
+//
+//  Created by James Anderson on 07/08/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
