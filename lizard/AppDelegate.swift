e17560785a8452cb7032fff5889a8db6751f2b7d
James Anderson <joji@ando.ws>
2025-08-13 09:52:21 +0100

    actual init

--- lizard/AppDelegate.swift
+++ lizard/AppDelegate.swift
@@ -1,30 +1,69 @@
-//
-//  AppDelegate.swift
-//  lizard
-//
-//  Created by James Anderson on 07/08/2025.
-//
-
 import Cocoa
+import AVFoundation
 
-@main
-class AppDelegate: NSObject, NSApplicationDelegate {
+class ClickableStatusBarButton: NSStatusBarButton {
+    var leftClickAction: (() -> Void)?
+    var rightClickMenu: NSMenu?
 
-    
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    override func mouseDown(with event: NSEvent) {
+        if event.type == .leftMouseDown {
+            leftClickAction?()
+        }
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    override func rightMouseDown(with event: NSEvent) {
+        if let menu = rightClickMenu {
+            NSMenu.popUpContextMenu(menu, with: event, for: self)
+        }
     }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-
 }
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+    var audioPlayer: AVAudioPlayer?
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem.button {
+            let customButton = ClickableStatusBarButton(frame: button.bounds)
+            statusItem.button?.superview?.addSubview(customButton)
+            statusItem.button?.removeFromSuperview()
+//            customButton.image = NSImage(systemSymbolName: "speaker.wave.2", accessibilityDescription: "lizard")
+            customButton.title = "🦎"
+            customButton.leftClickAction = { [weak self] in self?.playAudio() }
+            customButton.rightClickMenu = constructMenu()
+            customButton.target = self
+        }
+        
+        guard let audioURL = Bundle.main.url(forResource: "lizard", withExtension: "wav") else {
+            print("Audio file not found")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
+        } catch {
+            print("Failed to setup audio: \(error)")
+        }
+    }
+
+    @objc func playAudio() {
+        audioPlayer?.play()
+    }
+    
+    func constructMenu() -> NSMenu {
+        let menu = NSMenu()
+        menu.addItem(
+            withTitle: "Quit",
+            action: #selector(quitApp),
+            keyEquivalent: "q"
+        )
+        
+        return menu
+    }
+    
+    @objc func quitApp() {
+        NSApplication.shared.terminate(nil)
+    }
+}
